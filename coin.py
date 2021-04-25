cf9989af8b835112c5cc3f4e0c5308fdcfcb832d
lch3 <83167712+lch3@users.noreply.github.com>
2021-04-25 22:01:05 +0900

    Create coin.py

---
+++ coin.py
@@ -0,0 +1,36 @@
+import os
+import jwt
+import uuid
+import hashlib
+from urllib.parse import urlencode
+import pyupbit
+import numpy as np
+import requests
+import pandas as pd
+import time
+
+price = pyupbit.get_current_price("KRW-BTC")
+price_min=price
+price_max=price
+
+while True:
+    price = pyupbit.get_current_price("KRW-BTC")
+    while price > price_max and price > price_min:
+        if price < price_min*1.001 :
+            print('buy')
+            price_max=price
+            price_min=price
+    while price < price_max and price < price_min:
+        if price < price_max*0.999 :
+            print('sell')
+            price_max=price
+            price_min=price
+    if price_max < price :
+        price_max=price
+    if price_min > price :
+        price_min=price
+    print(price_max)
+    print(price)
+    print(price_min)
+    print('\n')
+    time.sleep(2)
